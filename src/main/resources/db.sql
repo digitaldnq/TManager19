b5325484a00b215aa73911edf45cdde76e454215
DEV41 <denqcr19@gmail.com>
2025-06-05 11:09:42 +0500

    add workers table

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -17,3 +17,13 @@ CREATE TABLE IF NOT EXISTS tasks (
                                      category_id BIGINT NOT NULL,
                                      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS workers (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    worker_name VARCHAR(255) NOT NULL,
+    status ENUM('QUEUED', 'IN_PROGRESS', 'COMPLETED', 'FAILED', 'CANCELLED') NOT NULL,
+    active_tasks INT DEFAULT 0,
+    last_heartbeat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    category_id BIGINT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
